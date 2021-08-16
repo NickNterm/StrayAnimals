119890c7c7e908ae9ac3a018f518b2579ad8608d
nicknt <nikolasderm@gmail.com>
2021-08-17 00:09:16 +0300

    paths changed. added Admin app

---
+++ AdminApp/app/src/main/java/com/iqsoft/strayanimalsadminapp/Constants.kt
@@ -0,0 +1,4 @@
+package com.iqsoft.strayanimalsadminapp
+
+object Constants {
+}
---
+++ AdminApp/app/src/main/java/com/iqsoft/strayanimalsadminapp/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.iqsoft.strayanimalsadminapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iqsoft.strayanimalsadminapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
