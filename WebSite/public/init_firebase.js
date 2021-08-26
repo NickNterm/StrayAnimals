b5b4c1dea3b61c0434218d77218c8bc7b6ff84ec
nicknt <nikolasderm@gmail.com>
2021-08-26 19:14:53 +0300

    add react native

--- WebSite/public/init_firebase.js
+++ WebSite/public/init_firebase.js
@@ -1,11 +1,13 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyClD0feYBVqMTzciMdZjYqQa7Er82zPQDs",
-    authDomain: "strayanimalsapp.firebaseapp.com",
-    projectId: "strayanimalsapp",
-    storageBucket: "strayanimalsapp.appspot.com",
-    messagingSenderId: "783963393864",
-    appId: "1:783963393864:web:c5b09e1d60a6ae0530ed7d",
-    measurementId: "G-QJ4C7L9L7K"
-  };
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBIjKRttnKNMgPk5s8kvfeOo_DkR_PiPxM",
+  authDomain: "strayanimalsapp.firebaseapp.com",
+  projectId: "strayanimalsapp",
+  storageBucket: "strayanimalsapp.appspot.com",
+  messagingSenderId: "783963393864",
+  appId: "1:783963393864:web:c5b09e1d60a6ae0530ed7d",
+  measurementId: "G-QJ4C7L9L7K"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
